6753499779e5b62e060bcc37d50546cc41e12a79
raficraft <raficraft@gmail.com>
2022-01-15 17:16:01 +0100

    add responsive design

--- src/index.js
+++ src/index.js
@@ -8,8 +8,41 @@ import guides from "guides_js_by_raficraft";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <header className="header">
+      <h1>Date Picker by Raficraft</h1>
+    </header>
+    <section className="main">
+      <App />
+    </section>
     <Pop></Pop>
+
+    <div className="social_icon--content">
+      <a
+        href="https://www.linkedin.com/in/raphael-parodi-a942ab1b0/"
+        target="_blank"
+        alt="My Linkedin profil"
+      ></a>
+      <a
+        href="https://inexpensive-chipmunk-220.notion.site/Hard-Skills-Soft-Skills-88964160557a441f9608d75c711ff06b"
+        target="_blank"
+        alt="My profil in Notion"
+      ></a>
+      <a
+        href="https://github.com/raficraft/GUIDES_JS"
+        target="_blank"
+        alt="Github repositories of this project"
+      ></a>
+      <a
+        href="https://app.assessfirst.com/_/profile/d53utubs-raphael-parodi"
+        target="_blank"
+        alt="Soft skill profil"
+      ></a>
+      <a
+        href="https://www.npmjs.com/package/guides_js_by_raficraft"
+        target="_blank"
+        alt="Package NPM"
+      ></a>
+    </div>
   </React.StrictMode>,
   document.getElementById("root")
 );
